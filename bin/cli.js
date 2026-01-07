68ebd3ee22f7c1554413b0d33444643c1b42eb45
caiqingzhou <caiqingzhou@clt.com>
2026-01-07 17:59:47 +0800

    fix: 修复Windows环境下使用ESM的兼容问题

--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-import { fileURLToPath } from 'url';
+import { fileURLToPath, pathToFileURL } from 'url';
 import { dirname, join } from 'path';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const mainModulePath = join(__dirname, "..", "dist", 'index.js');
 
-import(mainModulePath);
+import(pathToFileURL(mainModulePath).href);
